cea18bbbe0e9003e171898c4fc8c66b47c584a9a
Shileiz <shileiz@qti.qualcomm.com>
2015-08-03 14:36:27 -0700

    an ugly working one

--- MultiThread/writeABCD.cpp
+++ MultiThread/writeABCD.cpp
@@ -25,17 +25,20 @@ using namespace std;
 mutex mtx;
 
 condition_variable cvs[4];
+volatile bool flags[4] = { false };
 
-const int loop_count = 300;
+const int loop_count = 100;
 
 void WriteThread(int content, vector<vector<char>>& pipes,
-	             condition_variable& my_cv, condition_variable &next_cv){
-	int pipe_index= content - 'A';
+	condition_variable& my_cv, condition_variable &next_cv){
+	int pipe_index = content - 'A';
 	for (int i = 0; i < loop_count; ++i){
 		// lock
 		unique_lock<mutex> lk(mtx);
-		my_cv.wait(lk);
+		my_cv.wait(lk, [&](){return flags[content - 'A']; });
 		pipes[pipe_index].push_back(content);
+		flags[(content - 'A' + 1) % 4] = true;
+		flags[content - 'A'] = false;
 		next_cv.notify_one();
 		--pipe_index;
 		if (pipe_index < 0){
@@ -45,23 +48,28 @@ void WriteThread(int content, vector<vector<char>>& pipes,
 }
 
 int main(){
-	const int t_count = 4;
+	const int t_count = 4, th_count = 10;
 	thread write_threads[t_count];
-	vector<vector<char>> pipes(4, vector<char>());
-	for (int i = 0; i < t_count; ++i){
-		write_threads[i] = thread(WriteThread, int('A')+i, std::ref(pipes), 
-			                      std::ref(cvs[i]), std::ref(cvs[(i+1)%4]));
-	}
-	cvs[0].notify_one();
-	for (int i = 0; i < t_count; ++i){
-		write_threads[i].join();
-	}
-	for (auto & v : pipes){
-		for (auto e : v){
-			cout << e << " ";
+	for (auto j = 0; j < th_count; ++j){
+		vector<vector<char>> pipes(4, vector<char>());
+		for (int i = 0; i < t_count; ++i){
+			write_threads[i] = thread(WriteThread, int('A') + i, std::ref(pipes),
+				std::ref(cvs[i]), std::ref(cvs[(i + 1) % 4]));
 		}
-		cout << endl;
+		flags[0] = true;
+		cvs[0].notify_one();
+		for (int i = 0; i < t_count; ++i){
+			write_threads[i].join();
+		}
+		for (auto & v : pipes){
+			for (auto e : v){
+				cout << e << " ";
+			}
+			cout << endl;
+		}
+		flags[0] = false;
+		cout << "done ... " << endl;
+
 	}
-	cout << "done ... " << endl;
 	return 0;
 }
