19826937ace847391972502304470671af2fd812
tashia <tashia802@gmail.com>
2020-09-14 04:14:54 +0000

    Use msft script to setup container and some setting changes

--- Trie/main_testcpp.cpp
+++ Trie/main_testcpp.cpp
@@ -1,21 +1,26 @@
 #include <iostream>
 #include <vector>
 #include "trie.h"
+#include <deque>
 
 using namespace std;
 
 int main(){
-	vector<string> tstr{"caonimade", "caonimei", "cccc", "caonidayede"};
+	vector<string> tstr{"gggnimade", "gggnimei", "cccc", "gggnidayede"};
+	deque<int> intq{9, 7 , 3, 2, 9};
+	for(const auto i: intq){
+		cout<<i<<endl;
+	}
 	shared_ptr<Trie> trie = make_shared<Trie>(Trie());
 	for (auto e : tstr){
 		trie->Insert(e);
 	}
-	tstr.push_back("caoninainaia");
+	tstr.push_back("gggninainaia");
 	for (auto e : tstr){
 		cout << e << endl;
 		cout << trie->Search(e) << endl;
 	}
-	cout << trie->StartsWith("caoni") << endl;
+	cout << trie->StartsWith("gggni") << endl;
 
 	return 0;
 }
